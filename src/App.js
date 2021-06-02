355b98ef5428dccb55a9cba4dd49dfd3c6b3e84c
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-06-02 22:08:17 +0200

    header added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/ui/Header";
 
 function App() {
   return (
     <div className="Container">
-      Hello
+      <Header />
     </div>
   );
 }
