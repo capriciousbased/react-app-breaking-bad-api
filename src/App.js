082d08f8a42f351240b93523f08f5494ee7f510b
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-06-02 22:24:32 +0200

    data fetching option created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Header from "./components/ui/Header";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+
+      console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+    fetchItems();
+  }, []);
+
   return (
     <div className="Container">
       <Header />
